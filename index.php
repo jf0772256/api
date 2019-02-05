1e3c717f91745e7403348b03533b0260a43eca09
Jesse Fender <jf0772256@otc.edu>
2019-02-04 22:32:06 -0600

    text for index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>API Test Page</title>
+
+  <script src="./includes/scripts/js/jquery-3.3.1.min.js"></script>
+  <script src="./includes/scripts/js/api.js"></script>
+</head>
+<body>
+  <header>
+    <h1 style="text-align:center;"> API Test Page</h1>
+  </header>
+</body>
+</html>
