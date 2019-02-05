1a3b9dd14766426272a84be880c22f54236b94d4
Jesse Fender <jf0772256@otc.edu>
2019-02-04 20:18:34 -0600

    forces uppercase on method

--- includes/scripts/js/api.js
+++ includes/scripts/js/api.js
@@ -2,7 +2,7 @@
 function callAPI(url, data, contentType, method, success, error, completed) {
   url = (isUndefined(url) || isNull(url) || url == "") ? false : url;
   if (isBoolean(url)) return;
-  method = (isUndefined(method) || isNull(method) || method == "") ? 'GET' : method;
+  method = (isUndefined(method) || isNull(method) || method == "") ? 'GET' : method.toUpperCase();
   success = (!isFunction(success)) ? function(){} : success;
   error = (!isFunction(error)) ? function(){} : error;
   completed = (!isFunction(completed)) ? function(){} : completed;
