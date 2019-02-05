b621d82782a6e25d7e520ee8297b75eac577ec3e
Jesse Fender <jf0772256@otc.edu>
2019-02-04 22:32:33 -0600

    Initial startup of the api.php file

--- includes/scripts/php/api/api.php
+++ includes/scripts/php/api/api.php
@@ -2,5 +2,15 @@
   require_once('./../other/functions.php');
   require_once('./../other/autoloader.php');
 
+  $aj = isAjax();
+  $method = get_method();
+  $request = get_request_info();
+
+  if ($aj) {
+    # code...
+  } else {
+    echo "<html><head><title>403: Unauthorized</title></head><body><style>.center { text-align:center;}</style> <h1 class='center'>403:<small> Unauthorized</small></h1></body></html>";
+  }
+
 
 ?>
