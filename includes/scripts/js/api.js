e4a8c898a734d22a3846704ede1c2e31b98401b5
Jesse Fender <jf0772256@otc.edu>
2019-02-04 18:31:27 -0600

    added code to the handler

---
+++ includes/scripts/js/api.js
@@ -0,0 +1,20 @@
+
+function callAPI(url, data, contentType, method, success, error, completed) {
+  url = (isUndefined(url) || isNull(url) || url == "") ? false : url;
+  if (isBoolean(url)) return;
+  method = (isUndefined(method) || isNull(method) || method == "") ? 'GET' : method;
+  success = (!isFunction(success)) ? function(){} : success;
+  error = (!isFunction(error)) ? function(){} : error;
+  completed = (!isFunction(completed)) ? function(){} : completed;
+  if (window.jQuery == null) {
+    return;
+  }
+  $.ajax({
+    url: url,
+    cache: false,
+    method: method,
+    success: success,
+    error: error,
+    completed: completed
+  });
+}
