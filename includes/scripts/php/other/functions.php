66ea679579bca67fd21bb5ece1d10ea4851d3fd3
Jesse Fender <jf0772256@otc.edu>
2020-07-18 14:10:58 -0500

    update to functions, but this will likely need to be added to and modified to make even better
    This is a intermediate version of the functions file, more will be
    comming soon

--- includes/scripts/php/other/functions.php
+++ includes/scripts/php/other/functions.php
@@ -1,53 +1,94 @@
 <?php
 
 /**
- * Function to enforce xmlhttprequest...
+ * Function to read php pages into a variable to encode.
  *
- * @return boolean
+ * @param string $file Path to file to be included
+ * @return string String value of the file content
  */
-function isAjax()
+function file_to_var(string $file)
 {
-  return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+  global $user_info;
+  ob_start();
+  include($file);
+  return ob_get_clean();
 }
 
 /**
- * Returns the method used for the ajax call
+ * Gets the method used for the request, should only be one of four, GET, POST, PUT, and DELETE.
  *
  * @return string
  */
-function get_method()
+function get_method ()
 {
   return filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_ENCODED);
 }
 
 /**
- * Should return an array of values passed via ajax
+ * function that tests if a request is sent via ajax and returns either a true or false. only true request are process false requests return 403 always
  *
- * @return array
+ * @return boolean
  */
-function get_request_info()
+function isAJAX()
 {
-  if ($_SERVER['CONTENT_TYPE'] != 'application/json') {
-    if (count($_GET) > 1) {
+  if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && \strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Parses out the request values and returns them as an array
+ *
+ * @return array parameters array either from json or urlencoded values
+ */
+function get_request ()
+{
+  if ($_SERVER['CONTENT_TYPE'] != 'application/json'){
+    if (count($_GET) > 0) {
       return $_GET;
-    } elseif (count($_POST) > 1) {
+    } elseif (count($_POST) > 0) {
       return $_POST;
     } else {
-      $query_str = file_get_contents("php://input"); 
-      $array = array(); 
-      parse_str($query_str, $array); 
+      $query_str = file_get_contents("php://input");
+      $array = [];
+      parse_str($query_str, $array);
       return $array;
     }
   } else {
-    if (count($_GET) == 1) {
-      return json_decode($_GET[0], true);
-    } elseif (count($_POST) == 1) {
-      return json_decode($_POST[0], true);
+    if (!empty($_GET)){
+      reset($_GET);
+      $tmp = json_decode(key($_GET), true);
+      if (isset($tmp['values'])) {
+        $tmp['values'] = do_values_array_things($tmp['values']);
+      }
+      return $tmp;
+    } elseif (!empty($_POST)) {
+      $tmp = json_decode(key($_POST), true);
+      if (isset($tmp['values'])) {
+        $tmp['values'] = do_values_array_things($tmp['values']);
+      }
+      return $tmp;
     } else {
-      $date = file_get_contents("php://input");
-      return json_decode($date, true);
+      $sw = json_decode(file_get_contents("php://input"), true);
+      if (isset($sw['values'])) {
+        if (!is_array($sw['values'])){
+          $sw['values'] = do_values_array_things($sw['values']);
+        }
+      }
+      return $sw;
     }
   }
 }
 
-?>
+/**
+ * Takes stringified array and reverts it to an array again, use ',' as the value delimiter.
+ *
+ * @param string $instring Stringified array to return into an array.
+ * @return array The array of the stringified values.
+ */
+function do_values_array_things( string $instring)
+{
+  return explode(',', $instring);
+}
